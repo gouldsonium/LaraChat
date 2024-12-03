7766f8100236a535013551d13270e31fdc0dbe18
Stefan Gouldson <stefangouldson@gmail.com>
2024-12-03 23:25:45 +0000

    chat widget redesign

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import ChatInterface from '@/Components/Chat/ChatInterface.vue';
 </script>
 
 <template>
@@ -11,12 +11,8 @@ import Welcome from '@/Components/Welcome.vue';
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
-            </div>
+        <div class="max-w-7xl mx-auto">
+            <ChatInterface />
         </div>
     </AppLayout>
 </template>
