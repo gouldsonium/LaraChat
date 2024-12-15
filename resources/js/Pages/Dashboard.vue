6b8e360fb3f10ecefeb66507a504d9afccdc41a2
Stefan Gouldson <stefangouldson@gmail.com>
2024-12-15 02:00:30 +0000

    show balance on dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,9 +6,14 @@ import ChatInterface from '@/Components/Chat/ChatInterface.vue';
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Dashboard
-            </h2>
+            <div class="flex justify-between items-center">
+                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    Dashboard
+                </h2>
+                <p class="text-gray-800 dark:text-gray-200">
+                    ${{ Math.floor($page.props.auth.user.balance * 100) / 100}}
+                </p>
+            </div>
         </template>
 
         <div class="max-w-7xl mx-auto">
